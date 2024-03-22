6136ce4d701cec1f62a6961971629bec14d60116
zoyasiddiqui <zoya.siddiqui@mail.utoronto.ca>
2024-03-21 21:17:40 -0400

    added user interaction

--- quickstart.py
+++ quickstart.py
@@ -67,7 +67,52 @@ def main():
     print("\nCalendar List Access Role Retrieval")
     calendar_list_entry = service.calendarList().get(calendarId='zoyasdq.04@gmail.com').execute()
     print(calendar_list_entry['accessRole'])
+
+    # Getting all calendars for some user and listing them out
+    print("\nPick a category.")
+    page_token = None
+    all_calendars = []
+    count = 0
+    while True:
+        calendar_list = service.calendarList().list(pageToken=page_token).execute()
+        for calendar_list_entry in calendar_list['items']:
+            all_calendars.append(calendar_list_entry['summary'])
+            count += 1
+        page_token = calendar_list.get('nextPageToken')
+        if not page_token:
+            break
     
+    for i in range(0, count):
+      print(all_calendars[i])
+
+
+    #prompting for entry info : category, start time, end time
+    cur_cat = input("Which category would you like to work in? ")
+    while(cur_cat not in all_calendars):
+      print("This is not a valid option. Try again.")
+      cur_cat = input("Which category would you like to work in? ")
+
+    ready = input("Ready to start?")
+    if (ready):
+      start = datetime.datetime.now(datetime.timezone.utc)
+    else:
+      while(not ready):
+         ready = input("Ready to start?")
+        
+
+    done = input("Are you done working?")
+    if (done):
+      end = datetime.datetime.now(datetime.timezone.utc)
+    else:
+      while(not done):
+         done = input("Are you done working?")
+
+    print(start)
+    print(end)
+
+
+
+
 
   except HttpError as error:
     print(f"An error occurred: {error}")
