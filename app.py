70a14b373a69fb0f19e60fed8c293111b4b12609
zoyasiddiqui <zoya.siddiqui@mail.utoronto.ca>
2024-05-14 21:33:38 -0400

    finished application, fully functional

--- app.py
+++ app.py
@@ -35,6 +35,8 @@ def authenticate():
 
 def main():
     root = tk.Tk()
+    start_time = None
+    end_time = None
 
     def login():
         try:
@@ -48,23 +50,6 @@ def main():
 
             display(creds)
 
-            # service = build("calendar", "v3", credentials=creds)
-
-            # # Call the Calendar API
-            # now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
-            # print("Getting the upcoming 5 events")
-            # events_result = (
-            #     service.events()
-            #     .list(
-            #         calendarId="primary",
-            #         timeMin=now,
-            #         maxResults=5,
-            #         singleEvents=True,
-            #         orderBy="startTime",
-            #     )
-            #     .execute()
-            # )
-
         except Exception as e:
             messagebox.showerror("Error", f"An error occurred during authentication:\n{str(e)}")
             print(e)
@@ -73,20 +58,76 @@ def main():
     def display(creds):
         service = build("calendar", "v3", credentials=creds)
         page_token = None
+        all_cals = {}
         cal_btns = []
         while True:
             calendar_list = service.calendarList().list(pageToken=page_token).execute()
             for calendar_list_entry in calendar_list['items']:
-                cal_btns.append(tk.Button(main_frame, text=calendar_list_entry['summary'], command=None))
+                cal = calendar_list_entry['summary']
+                all_cals[cal] = calendar_list_entry['id']
+                cal_btns.append(tk.Button(main_frame, text=cal, command=None))
             page_token = calendar_list.get('nextPageToken')
             if not page_token:
                 break
         
         row_ctr = 2
         for b in cal_btns:
+            category = b.cget("text")
+            b.config(command=lambda cat=category: act(cat, service, all_cals))
             b.grid(row=row_ctr, column=0, pady = 5,sticky="w")
             row_ctr += 1
 
+    def act(category, service, all_cals):
+
+        title = tk.Entry(main_frame)
+        title.insert(0, "Event Title")
+        title.grid(row=2, column=1, padx=10, pady=5, sticky="e")
+        alert = tk.Label(main_frame, text="Timer has started")
+
+        start = tk.Button(main_frame, text="Start", command=lambda: get_start_time(alert))
+        end = tk.Button(main_frame, text="End", command=lambda: wrapup(service, all_cals[category], title.get(), alert))
+        start.grid(row=3, column=1, padx=5, pady=5, sticky="e")
+        end.grid(row=4, column=1, padx=5, pady=5, sticky="e")
+
+    def get_start_time(alert):
+        global start_time
+        start_time = datetime.datetime.now(datetime.timezone.utc)
+        hours_added = datetime.timedelta(hours=-7)
+        start_time += hours_added
+
+        alert.grid(row=5, column=1, padx=5, pady=5, sticky="w")
+    
+    def wrapup(service, id, title, alert):
+        global start_time
+        global end_time
+
+        if start_time != None:
+            end_time = datetime.datetime.now(datetime.timezone.utc)
+            hours_added = datetime.timedelta(hours=-7)
+            end_time += hours_added
+
+            start_str = start_time.strftime("%Y-%m-%dT%H:%M:%S-07:00")
+            end_str = end_time.strftime("%Y-%m-%dT%H:%M:%S-07:00")
+
+            event = {
+                'summary': title,
+                'start': {
+                    'dateTime': start_str,
+                    'timeZone': 'America/Los_Angeles',
+                },
+                'end': {
+                    'dateTime': end_str,
+                    'timeZone': 'America/Los_Angeles',
+                }
+            }
+
+            event = service.events().insert(calendarId=id, body=event).execute()
+
+            alert.grid_remove()
+            title.insert(0, "Event Title")
+        else:
+            messagebox.showerror("Error", f"You have not started tracking yet. You must click start.")
+            root.quit()
 
     login_frame = tk.Frame(root)
     login_frame.pack()
